12d25292fdcfd1b2d6ce0e100f34e56a5ec43e74
kndxhz <kndxhz@163.com>
2024-10-02 10:59:49 +0800

    按钮改为全线程操作

--- Let's happy/Form1.cs
+++ Let's happy/Form1.cs
@@ -3,8 +3,8 @@ using System.IO;
 using System.Reflection;
 using System.Diagnostics;
 using System.Security.Principal;
-using System.Security.Cryptography;
-using System.Security.Policy;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Let_s_happy
 {
@@ -31,17 +31,20 @@ namespace Let_s_happy
                             MessageBoxButtons.OK,
                             MessageBoxIcon.Information);
             timer1.Start(); // 启动计时器
-            static bool IsRunningAsAdministrator()
-            {
-                WindowsIdentity identity = WindowsIdentity.GetCurrent();
-                WindowsPrincipal principal = new WindowsPrincipal(identity);
-                return principal.IsInRole(WindowsBuiltInRole.Administrator);
-            }
-            if (IsRunningAsAdministrator() == false)
+
+            // 检查管理员权限
+            if (!IsRunningAsAdministrator())
             {
                 MessageBox.Show("请以管理员身份运行本程序");
                 Application.Exit();
             }
+        }
+
+        private static bool IsRunningAsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -61,187 +64,185 @@ namespace Let_s_happy
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // 获取应用程序的运行目录
-            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string outputPath = Path.Combine(appDirectory, "ntsd.exe");
-
-            // 将资源文件保存为物理文件
-            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.ntsd.exe"))
+            Task.Run(() =>
             {
-                if (resourceStream != null)
+                string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                string outputPath = Path.Combine(appDirectory, "ntsd.exe");
+
+                using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.ntsd.exe"))
                 {
-                    using (var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                    if (resourceStream != null)
                     {
-                        resourceStream.CopyTo(fileStream);
+                        using (var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                        {
+                            resourceStream.CopyTo(fileStream);
+                        }
                     }
                 }
 
-
-            }
-            // 通过进程名获取所有匹配的进程
-            Process[] processes = Process.GetProcessesByName("StudentMain");
-            string pid = "";
-            if (processes.Length > 0)
-            {
-                foreach (Process process in processes)
+                Process[] processes = Process.GetProcessesByName("StudentMain");
+                string pid = "";
+                if (processes.Length > 0)
                 {
-                    MessageBox.Show($"进程名: {process.ProcessName}, PID: {process.Id}");
-                    pid = process.Id.ToString();
-                }
-            }
-            else
-            {
-                MessageBox.Show("未找到极域进程");
-                return;
-            }
-            // 定义要执行的命令和参数
-            string command = "ntsd.exe"; // 替换为要执行的命令
-            string arguments = $"-c q -p {pid}"; // 替换为命令的参数
-
-            // 创建一个新的进程
-            ProcessStartInfo processStartInfo = new ProcessStartInfo
-            {
-                FileName = "cmd.exe", // 使用 cmd.exe 执行命令
-                Arguments = $"/c {command} {arguments}", // /c 参数表示在执行完命令后关闭命令窗口
-                UseShellExecute = true, // 使用系统外壳程序启动
-                Verb = "runas", // 以管理员身份运行
-                WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory // 设置当前工作目录为程序运行目录
-            };
-
-            try
-            {
-                // 启动进程
-                Process process = Process.Start(processStartInfo);
-                process.WaitForExit(); // 等待命令执行完成
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"发生错误: {ex.Message}");
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            // 获取应用程序的运行目录
-            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string outputPath = Path.Combine(appDirectory, "sethc.exe");
-            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.sethc.exe"))
-            {
-                if (resourceStream != null)
-                {
-                    using (var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                    foreach (Process process in processes)
                     {
-                        resourceStream.CopyTo(fileStream);
+                        this.Invoke(new Action(() =>
+                        {
+                            MessageBox.Show($"进程名: {process.ProcessName}, PID: {process.Id}");
+                        }));
+                        pid = process.Id.ToString();
                     }
                 }
-
-
-                var processInfo = new ProcessStartInfo
+                else
                 {
-                    FileName = "explorer.exe",
-                    Arguments = "C:\\Windows\\System32",
-                    Verb = "runas", // 以管理员身份运行
+                    this.Invoke(new Action(() => { MessageBox.Show("未找到极域进程"); }));
+                    return;
+                }
+
+                string command = "ntsd.exe";
+                string arguments = $"-c q -p {pid}";
+
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/c {command} {arguments}",
                     UseShellExecute = true,
-                    CreateNoWindow = true
+                    Verb = "runas",
+                    WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory
                 };
 
                 try
                 {
-                    Process.Start(processInfo);
+                    Process process = Process.Start(processStartInfo);
+                    process.WaitForExit();
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show($"Error: {ex.Message}");
+                    this.Invoke(new Action(() => { MessageBox.Show($"发生错误: {ex.Message}"); }));
                 }
+            });
+        }
 
-                MessageBox.Show("已释放，请手动替换");
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                string outputPath = Path.Combine(appDirectory, "sethc.exe");
 
-            }
+                using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.sethc.exe"))
+                {
+                    if (resourceStream != null)
+                    {
+                        using (var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                        {
+                            resourceStream.CopyTo(fileStream);
+                        }
+                    }
+
+                    var processInfo = new ProcessStartInfo
+                    {
+                        FileName = "explorer.exe",
+                        Arguments = "C:\\Windows\\System32",
+                        Verb = "runas",
+                        UseShellExecute = true,
+                        CreateNoWindow = true
+                    };
+
+                    try
+                    {
+                        Process.Start(processInfo);
+                    }
+                    catch (Exception ex)
+                    {
+                        this.Invoke(new Action(() => { MessageBox.Show($"Error: {ex.Message}"); }));
+                    }
+
+                    this.Invoke(new Action(() => { MessageBox.Show("已释放，请手动替换"); }));
+                }
+            });
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.sethc.exe"))
+            Task.Run(() =>
             {
-                if (resourceStream != null)
+                using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.sethc.exe"))
                 {
-                    using (var fileStream = new FileStream("C:\\system.exe", FileMode.Create, FileAccess.Write))
+                    if (resourceStream != null)
                     {
-                        resourceStream.CopyTo(fileStream);
+                        using (var fileStream = new FileStream("C:\\system.exe", FileMode.Create, FileAccess.Write))
+                        {
+                            resourceStream.CopyTo(fileStream);
+                        }
                     }
                 }
-            }
-            MessageBox.Show("已释放至C盘根目录");
+                this.Invoke(new Action(() => { MessageBox.Show("已释放至C盘根目录"); }));
+            });
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            // 创建带有警告图标的消息框
-            DialogResult result = MessageBox.Show(
-                "本二级软件由@ZiHaoSaMa66开发\n我只是做了融合\n感谢这位大佬\n点击是将访问这个项目的GitHub主页",
-                "提示",
-                MessageBoxButtons.YesNo,
-                MessageBoxIcon.Warning
-            );
-
-            // 判断用户点击了“是”
-            if (result == DialogResult.Yes)
+            Task.Run(() =>
             {
-                // 打开GitHub主页
-                Process.Start(new ProcessStartInfo("https://github.com/ZiHaoSaMa66/OsEasy-ToolBox/") { UseShellExecute = true });
-                return;
-            }
-            // 获取应用程序的运行目录
-            string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
-            string outputPath = Path.Combine(appDirectory, "ToolBox 1.7 RC.exe");
+                DialogResult result = MessageBox.Show(
+                    "本二级软件由@ZiHaoSaMa66开发\n我只是做了融合\n感谢这位大佬\n点击是将访问这个项目的GitHub主页",
+                    "提示",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
 
-            // 将资源文件保存为物理文件
-            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.ToolBox 1.7 RC.exe"))
-            {
-                if (resourceStream != null)
+                if (result == DialogResult.Yes)
                 {
-                    using (var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                    Process.Start(new ProcessStartInfo("https://github.com/ZiHaoSaMa66/OsEasy-ToolBox/") { UseShellExecute = true });
+                    return;
+                }
+
+                string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                string outputPath = Path.Combine(appDirectory, "ToolBox 1.7 RC.exe");
+
+                using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.ToolBox 1.7 RC.exe"))
+                {
+                    if (resourceStream != null)
                     {
-                        resourceStream.CopyTo(fileStream);
+                        using (var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                        {
+                            resourceStream.CopyTo(fileStream);
+                        }
                     }
                 }
-            }
-            outputPath = Path.Combine(appDirectory, "ScreenRender_Helper.exe");
 
-            // 将资源文件保存为物理文件
-            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.ScreenRender_Helper.exe"))
-            {
-                if (resourceStream != null)
+                outputPath = Path.Combine(appDirectory, "ScreenRender_Helper.exe");
+
+                using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Let_s_happy.Resources.ScreenRender_Helper.exe"))
                 {
-                    using (var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                    if (resourceStream != null)
                     {
-                        resourceStream.CopyTo(fileStream);
+                        using (var fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                        {
+                            resourceStream.CopyTo(fileStream);
+                        }
                     }
                 }
-            }
 
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "ToolBox 1.7 RC.exe",
+                    UseShellExecute = true,
+                    Verb = "runas",
+                    WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory
+                };
 
-            // 创建一个新的进程
-            ProcessStartInfo processStartInfo = new ProcessStartInfo
-            {
-                FileName = "ToolBox 1.7 RC.exe", // 使用 cmd.exe 执行命令
-                Arguments = "", // /c 参数表示在执行完命令后关闭命令窗口
-                UseShellExecute = true, // 使用系统外壳程序启动
-                Verb = "runas", // 以管理员身份运行
-                WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory // 设置当前工作目录为程序运行目录
-            };
-
-            try
-            {
-                // 启动进程
-                Process process = Process.Start(processStartInfo);
-                process.WaitForExit(); // 等待命令执行完成
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"发生错误: {ex.Message}");
-            }
+                try
+                {
+                    Process process = Process.Start(processStartInfo);
+                    process.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    this.Invoke(new Action(() => { MessageBox.Show($"发生错误: {ex.Message}"); }));
+                }
+            });
         }
     }
 }
-
