81b3bea6b0746dbcb9a84e31881a7c60036b4d60
kndxhz <kndxhz@163.com>
2024-11-16 00:57:00 +0800

    完成基本ui设计 完成一言

--- OsEasy_Cloud_ToolBox/Form1.Designer.cs
+++ OsEasy_Cloud_ToolBox/Form1.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(18, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "一言";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "关闭监控";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(254, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(225, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(254, 99);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(225, 50);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(492, 170);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "OsEasy_Cloud_ToolBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- OsEasy_Cloud_ToolBox/Form1.cs
+++ OsEasy_Cloud_ToolBox/Form1.cs
@@ -1,20 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OsEasy_Cloud_ToolBox
 {
     public partial class Form1 : Form
     {
+        // 存储一系列的句子
+        private string[] sentences = new string[]
+        {
+            "《机课时间管理》",
+            "开源造福人类",
+            "锟斤拷烫烫烫",
+            "if...else...仙人！",
+            "While True仙人",
+            "不要自己造轮子！"
+        };
+
+        private int currentSentenceIndex = 0; // 当前显示的句子索引
+        private int currentCharIndex = 0; // 当前句子的字符索引
+        private Timer typingTimer;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 弹出程序信息框
+            MessageBox.Show("本程序由可耐的小伙纸开发\n以GPL V3协议开源\n感谢使用",
+                "程序信息",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; // 不允许调整大小
+            this.label1.Text = ""
+            // 初始化定时器
+            typingTimer = new Timer();
+            typingTimer.Interval = 100; // 设置每次显示字符的间隔（100毫秒）
+            typingTimer.Tick += TypingTimer_Tick;
+
+            // 开始打字效果
+            typingTimer.Start();
+        }
+
+        // 定时器事件：每次触发时，逐个显示字符
+        private void TypingTimer_Tick(object sender, EventArgs e)
+        {
+            // 判断当前句子是否已显示完
+            if (currentCharIndex < sentences[currentSentenceIndex].Length)
+            {
+                // 将下一个字符添加到 label1
+                this.label1.Text += sentences[currentSentenceIndex][currentCharIndex];
+                currentCharIndex++; // 移动到下一个字符
+            }
+            else
+            {
+                // 当前句子显示完后，暂停一段时间
+                typingTimer.Stop();
+
+                // 设置间隔时间后切换到下一个句子
+                Timer switchSentenceTimer = new Timer();
+                switchSentenceTimer.Interval = 1000; // 切换句子的间隔（1000毫秒）
+                switchSentenceTimer.Tick += (s, args) =>
+                {
+                    // 重新设置定时器，显示下一个句子
+                    switchSentenceTimer.Stop();
+                    currentCharIndex = 0; // 重置字符索引
+                    currentSentenceIndex++; // 切换到下一个句子
+                    if (currentSentenceIndex >= sentences.Length)
+                    {
+                        currentSentenceIndex = 0; // 如果到了最后一条，重新从头开始
+                    }
+
+                    // 清空 label 并开始新的打字效果
+                    this.label1.Text = "";
+                    typingTimer.Start();
+                };
+                switchSentenceTimer.Start(); // 启动切换句子的定时器
+            }
         }
     }
 }
